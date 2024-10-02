8cbac9069b7b93e545120b2053531a71f2fce06a
Dragomir J <dj20180232@student.fon.bg.ac.rs>
2024-10-03 01:41:23 +0200

    Fix the delay in the Arduino script

--- arduino/arduino_sample/mq2_co_reader.ino
+++ arduino/arduino_sample/mq2_co_reader.ino
@@ -16,5 +16,6 @@ void loop(){
   lpg = mq2.readLPG();
   co  = mq2.readCO();
   Serial.println(co); //Serial output is read by Raspberry Pi
+  delay(1000); //Wait for 1 second before the next read
 } //LOOP
 
